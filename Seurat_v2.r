bbb19b3d949bc2a692fc133dc2ff85f61d00eb0a
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2023-04-12 13:14:04 +0800

    Create Seurat_v2.r

---
+++ Seurat_v2.r
@@ -0,0 +1,91 @@
+rm(list=ls())
+setwd('D:/desk/wkdir/scRNA')
+library(dplyr)
+library(Seurat)
+library(patchwork)
+# 查看文件，
+list.files('pbmc3k_filtered_gene_bc_matrices/filtered_gene_bc_matrices/hg19')
+# 数据导入
+pbmc.counts <- Read10X(data.dir = "pbmc3k_filtered_gene_bc_matrices/filtered_gene_bc_matrices/hg19")
+# 创建Seurat对象
+pbmc <- CreateSeuratObject(counts = pbmc.counts)
+pbmc
+str(pbmc)
+
+# 计算线粒体read的百分比
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+# 显示前5个细胞的质控指标
+head(pbmc@meta.data, 5)
+
+plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+p1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")
+p2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+CombinePlots(plots = list(p1, p2))
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+# 识别前2000个特征
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+# 识别前10的高异质性基因
+top10 <- head(VariableFeatures(pbmc), 10)
+
+# 绘图看看
+plot1 <- VariableFeaturePlot(pbmc)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+CombinePlots(plots = list(plot1, plot2))
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+head(pbmc[["RNA"]]@scale.data,5)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+print(pbmc[["pca"]], dims = 1:5, nfeatures = 5)
+# 绘图
+VizDimLoadings(pbmc, dims = 1:2, reduction = "pca")
+DimPlot(pbmc, reduction = "pca")
+DimHeatmap(pbmc, dims = 1, cells = 500, balanced = TRUE)
+DimHeatmap(pbmc, dims = 1:15, cells = 500, balanced = TRUE)
+pbmc <- JackStraw(pbmc, num.replicate = 100)
+pbmc <- ScoreJackStraw(pbmc, dims = 1:20)
+# 绘图看看
+JackStrawPlot(pbmc, dims = 1:15)
+pbmc <- FindNeighbors(pbmc, dims = 1:10)
+pbmc <- FindClusters(pbmc, resolution = 0.5)
+# 查看前5聚类
+head(Idents(pbmc), 5)
+# 使用UMAP聚类
+pbmc <- RunUMAP(pbmc, dims = 1:10)
+DimPlot(pbmc, reduction = "umap")
+# 显示在聚类标签
+DimPlot(pbmc, reduction = "umap", label = TRUE)
+# 使用TSNE聚类
+pbmc <- RunTSNE(pbmc, dims = 1:10)
+DimPlot(pbmc, reduction = "tsne")
+# 显示在聚类标签
+DimPlot(pbmc, reduction = "tsne", label = TRUE)
+# 发现聚类一的所有biomarkers
+cluster1.markers <- FindMarkers(pbmc, ident.1 = 1, min.pct = 0.25)
+head(cluster1.markers, n = 5)
+
+# 查找将聚类5与聚类0和3区分的所有标记
+cluster5.markers <- FindMarkers(pbmc, ident.1 = 5, ident.2 = c(0, 3), min.pct = 0.25)
+head(cluster5.markers, n = 5)
+
+# 与所有其他细胞相比，找到每个簇的标记，仅报告阳性细胞
+pbmc.markers <- FindAllMarkers(pbmc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+pbmc.markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC)
+cluster1.markers <- FindMarkers(pbmc, ident.1 = 0, logfc.threshold = 0.25, test.use = "roc", only.pos = TRUE)
+# 绘图看看
+VlnPlot(pbmc, features = c("MS4A1", "CD79A"))
+# 使用原始count绘制
+VlnPlot(pbmc, features = c("NKG7", "PF4"), slot = "counts", log = TRUE)
+FeaturePlot(pbmc, features = c("MS4A1", "GNLY", "CD3E", "CD14", "FCER1A", "FCGR3A", "LYZ", "PPBP", "CD8A"))
+RidgePlot(pbmc, features = c("MS4A1", "CD79A"))
+DotPlot(pbmc, features = c("MS4A1", "CD79A"))
+top10 <- pbmc.ers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+DoHeatmap(pbmc, features = top10$gene) + NoLegend()
+new.cluster.ids <- c("Naive CD4 T", "Memory CD4 T", "CD14+ Mono", "B", "CD8 T", "FCGR3A+ Mono", 
+                     "NK", "DC", "Platelet")
+names(new.cluster.ids) <- levels(pbmc)
+pbmc <- RenameIdents(pbmc, new.cluster.ids)
+DimPlot(pbmc, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+
