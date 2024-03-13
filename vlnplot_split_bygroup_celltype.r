9181373777ab250ec3d07d58f6f1f4726b736835
JokerX6696 <313445042@qq.com>
2024-03-13 11:08:01 +0800

    添加显著性标记

--- vlnplot_split_bygroup_celltype.r
+++ vlnplot_split_bygroup_celltype.r
@@ -1,5 +1,6 @@
 library(Seurat)
 library(ggplot2)
+library(ggpubr)
 library(reshape2)
 rds='/gpfs/oe-scrna/zhengfuxing/Project/scRNA/DZOE2023121146_human/20240310/rds/all_celltype.rds'
 obj = readRDS(rds)
@@ -14,7 +15,15 @@ p <- ggplot(df,aes(x=celltype,y=exp,fill=group))+
             geom_boxplot(width=0.05,position=position_dodge(0.9))+
             labs(x="",y="Expression", title=gene) +
             theme_classic()+
-            theme(plot.title = element_text(hjust = 0.5))
+            theme(plot.title = element_text(hjust = 0.5))+
+            stat_compare_means(aes(group=group),                       #按分组进行统计检验
+                     method = "wilcox.test",
+                     paired = F,                             #非配对t检验
+                     symnum.args = list(cutpoint=c(0,0.001,0.01,0.05,1),
+                                        symbols=c("***","**","*","ns")),
+                     label = "p.signif",
+                     #label.y = location$Proportion+0.02,      #添加显著性符号的位置
+                     size=4.5)
 
 ggsave(p,device='png',filename='violinplot.png')
 ggsave(p,device='pdf',filename='violinplot.pdf')
