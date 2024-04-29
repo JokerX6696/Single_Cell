6baca92ec06799610375a4e439124fcc17354b69
JokerX6696 <313445042@qq.com>
2024-04-29 14:01:38 +0800

    快速平方根算法

---
+++ qsqrt.c
@@ -0,0 +1,17 @@
+float OE_Q_sqrt( float number )
+{
+    long i;
+    float x2, y;
+    int fix = 0;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y  = number;
+    i  = * ( long * ) &y;                       // evil floating point bit level hacking
+    i  = 0x1fc00000 + fix + ( i >> 1 );               // what the fuck? 
+    y  = * ( float * ) &i;
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//  y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+    return y;
+}
