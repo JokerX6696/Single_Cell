3fe74db263978689d2954bba72342a1f89c641aa
JokerX6696 <313445042@qq.com>
2025-01-08 17:13:23 +0800

    封装为函数

--- get_gseafile.r
+++ get_gseafile.r
@@ -1,31 +1,31 @@
-obj = rd("/gpfs/oe-scrna/further_analysis/scRNA/Mobi/DOE202212736/20240703/3.Keratinocytes/3.1.newcelltype/newcelltype/seurat.h5seurat")
+obj = rd("/gpfs/oe-scrna/further_analysis/scRNA/10x/DZOE2023092903/20250107/1.featureplot/rds/data_ob_v3.rds")
 
 
+get_gsea_config = function(object,case,control){
+    if(any(! rownames(object@meta.data) == colnames(object@assays$RNA@data))){
+        stop('order error')
+    }
+    # 矩阵
+    exp = object@assays$RNA@data
+    exp = normalize_mtx(exp)
+    names(exp)[1] = 'genes'
+    outname = paste(case,control,'exp',sep='_')
+    wq(exp,prefix=outname)
 
-if(any(! rownames(obj@meta.data) == colnames(obj@assays$RNA@data))){
-    stop('order error')
+    # cls 文件
+    sy = case
+    dz = control
+    cls = data.frame(col1="",col2="",col3=object@meta.data$group)
+    cls$col1[1] = length(object@meta.data$group)
+    cls$col1[2] = 2
+    cls$col1[3] = 1
+
+    cls$col2[1] = '#'
+    cls$col2[2] = sy
+    cls$col2[3] = dz
+
+    cls = t(cls)
+    outname = paste(case,control,'exp.cls',sep='_')
+    write.table(cls,file='exp.cls',sep=' ',quote=F,col.names=F,row.names=F)
 }
-# 矩阵
-exp = obj@assays$RNA@data
-exp = normalize_mtx(exp)
-names(exp)[1] = 'genes'
-wq(exp,'exp')
-
-# cls 文件
-sy = 'rosacea'
-dz = 'control'
-cls = data.frame(col1="",col2="",col3=obj@meta.data$group)
-cls$col1[1] = length(obj@meta.data$group)
-cls$col1[2] = 2
-cls$col1[3] = 1
-
-cls$col2[1] = '#'
-cls$col2[2] = sy
-cls$col2[3] = dz
-
-cls = t(cls)
-
-write.table(cls,file='exp.cls',sep=' ',quote=F,col.names=F,row.names=F)
-
-
 
